9df7251eb882283499de012ea3dbeaa3938deadf
John Tinetti <john.tinetti@target.com>
2016-05-17 23:21:27 -0500

    upgraded to angular2-rc1

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # project_cars_leaderboard_server_angular
+
+h3. Running Locally
+Requires node 4.4
+```
+nvm use 4.4
+```
+
+```
+npm install
+npm start
+```
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,7 @@
-import {Component} from 'angular2/core';
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'my-app',
+  selector: 'my-app',
     template: '<h1>Project CARS Leaderboard</h1>'
 })
 export class AppComponent { }
-
